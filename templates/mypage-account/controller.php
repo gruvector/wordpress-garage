4f75eef9c6ab9d5d911ab92719d32c458f4fba87
gruvector <vector.gru316@gmail.com>
2024-01-23 00:45:40 +0900

    correction 12

--- templates/mypage-account/controller.php
+++ templates/mypage-account/controller.php
@@ -2,7 +2,11 @@
 
 require_once ABSPATH . 'wp-content/themes/sango-theme-child-garage/templates/_common/abstarct-template-mypage-controller.php';
 class Gm_Mypage_Account_Controller extends Abstract_Template_Mypage_Controller
+
 {
+    public $account_attr_records = [];
+    public $account_other = false;
+
     protected function setting()
     {
         parent::setting();
@@ -26,7 +30,6 @@ class Gm_Mypage_Account_Controller extends Abstract_Template_Mypage_Controller
         if (isset($_POST['process']) && $_POST['process'] == 'regist') {
             $this->regist($this->url_params());
         }
-
         // -------------------
         // 画面描画
         // -------------------
@@ -35,8 +38,8 @@ class Gm_Mypage_Account_Controller extends Abstract_Template_Mypage_Controller
         if ($this->mode == 'confirm') {
             $this->set_input_params($this->url_params());
         }
-        $this->availability_records = $this->wpdb->get_results("SELECT ID, nm FROM {$this->wpdb->prefix}gmm_account_attr order by priority");
-        // 画面描画
+        $this->account_attr_records = $this->wpdb->get_results("SELECT ID, nm FROM {$this->wpdb->prefix}gmm_account_attr order by priority");
+
         $this->render();
     }
 
@@ -46,6 +49,45 @@ class Gm_Mypage_Account_Controller extends Abstract_Template_Mypage_Controller
         // 入力チェック
         // -----------------
         $validation = new Gm_Validation($params);
+        $validation->required([
+            ['key' => 'nm', 'name' => '名前',],
+            ['key' => 'kana', 'name' => 'カナ',],
+            ['key' => 'email', 'name' => 'メールアドレス',],
+            ['key' => 'phone', 'name' => '電話番号',],
+            ['key' => 'postal_code', 'name' => '郵便番号',],
+            ['key' => 'address_1', 'name' => '都道府県',],
+            ['key' => 'address_2', 'name' => '市区町村',],
+            ['key' => 'address_3', 'name' => '地番',],
+            ['key' => 'account_attr_id', 'name' => 'アカウント属性',],
+         ]);
+        $validation->length([
+           ['key' => 'nm', 'name' => '名前', 'len' => 255],
+           ['key' => 'kana', 'name' => 'カナ', 'len' => 255],
+         ]);
+        $validation->zen_katakana([
+           ['key' => 'kana', 'name' => 'カナ',],
+         ]);
+        $validation->email([
+           ['key' => 'email', 'name' => 'メールアドレス',],
+         ]);
+        $validation->phone([
+          ['key' => 'phone', 'name' => '電話番号',],
+        ]);
+
+        $validation->numeric([
+          ['key' => 'account_attr_id', 'name' => 'アカウント属性',],
+        ]);
+
+        if ($params['account_attr_id'] == '9') {
+            $validation->required([
+                ['key' => 'account_attr_other', 'name' => 'アカウント属性その他',],
+            ]);
+            $validation->length([
+                ['key' => 'account_attr_other', 'name' => 'アカウント属性その他', 'len' => 255],
+            ]);
+            
+            $this->account_other = true;
+        }
 
         $errors = $validation->errors();
         if (!empty($errors)) {
@@ -65,37 +107,45 @@ class Gm_Mypage_Account_Controller extends Abstract_Template_Mypage_Controller
             $url = $url . '?mode=confirm&v=' . urlencode(Gm_Util::encrypt(json_encode($params, JSON_UNESCAPED_UNICODE)));
         }
 
+        var_dump($url);
         // 画面遷移
         header('Location: ' . $url);
-        exit();
     }
-
 
     private function regist($params)
     {
         // -----------------
         // データ登録
         // -----------------
+        $postal_code = $params['postal_code1'] ."-". $params['postal_code2'];
         $this->wpdb->update(
-            $this->wpdb->prefix.'gmt_account',
+            $this->wpdb->prefix.'gmt_account_tmp',
             [
-                'nm' => $params['account_nm'],
-                'email' => $params['account_email'],
-                'phone' => $params['account_tel'],
-                'address' => $params['account_address'],
-                'nm' => $params['account_nm'],
-                'nm' => $params['account_nm'],
-            ], ['ID' => $_SESSION['account_id']]
+                'nm' => $params['nm'],
+                'kana' => $params['kana'],
+                'email' => $params['email'],
+                'phone' => $params['phone'],
+                'postal_code' => $postal_code,
+                'address_1' => $params['address_1'],
+                'address_2' => $params['address_2'],
+                'address_3' => $params['address_3'],
+                'address_4' => $params['address_4'],
+                'account_attr_id' => $params['account_attr_id'],
+                'account_attr_other' => isset($params['account_attr_other']) ? $params['account_attr_other'] : null,
+                'apply_memo' => $params['apply_memo'],
+            ],
+            [
+                'ID' => $_SESSION['account_id']
+            ]
         );
 
-        
 
         // -----------------
         // 画面遷移
-
+        // -----------------
+        // 画面遷移
         $url = explode('?', Gm_Util::get_url())[0];
         header('Location: ' . $url . '?mode=completed');
         exit();
     }
-
 }
--- templates/mypage-account/view.php
+++ templates/mypage-account/view.php
@@ -9,34 +9,25 @@ if (! defined('ABSPATH')) {
 <div class="gm-custom-wrap">
 <?php if($this->mode == '') :?>
     <form id="gm-page-form" method="POST">
-        <input type="text" style="display:none" palceholder="Enter対策">
+        <input type="text" style="display:none" placeholder="Enter対策">
         <input type="hidden" name="process" value="check">
         <div class="gm-input-table-wrap">
             <table class="gm-input-table">
                 <tr>
-                    <th><div>お名前</div></th>
+                    <th><div>名前</div></th>
                     <td>
                         <div>
-                            <input class="gm-input" type="text" name="account_nm" value="<?php echo $this->get_input_param('account_nm') ?>" 
-                            data-gm-required>
+                            <input class="gm-input" type="text" name="nm" value="<?php echo $this->get_input_param('nm') ?>" 
+                            data-gm-required data-gm-length="255">
                         </div>
                     </td>
                 </tr>
                 <tr>
-                    <th><div>住所</div></th>
+                    <th><div>カナ</div></th>
                     <td>
                         <div>
-                            <input class="gm-input" type="text" name="account_address" value="<?php echo $this->get_input_param('account_address') ?>"  
-                            data-gm-required >
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <th><div>電話番号</div></th>
-                    <td>
-                        <div>
-                            <input class="gm-input" type="tel" name="account_tel" value="<?php echo $this->get_input_param('account_tel') ?>"  
-                            data-gm-required data-gm-phone>
+                            <input class="gm-input" type="text" name="kana" value="<?php echo $this->get_input_param('kana') ?>" 
+                            data-gm-required data-gm-length="255" data-gm-zen-katakana>
                         </div>
                     </td>
                 </tr>
@@ -44,37 +35,95 @@ if (! defined('ABSPATH')) {
                     <th><div>メールアドレス</div></th>
                     <td>
                         <div>
-                            <input class="gm-input" type="email" name="account_email" value="<?php echo $this->get_input_param('account_email') ?>"  
+                            <input class="gm-input" type="text" name="email" value="<?php echo $this->get_input_param('email') ?>"  
+                            data-gm-required data-gm-email>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th><div>電話番号</div></th>
+                    <td>
+                        <div>
+                            <input class="gm-input" type="text" name="phone" value="<?php echo $this->get_input_param('phone') ?>"  
+                            data-gm-required data-gm-phone>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th><div>郵便番号</div></th>
+                    <td>
+                        <div class="gm-zipcode-part">
+                            <input class="gm-input2" type="text" name="postal_code" value="<?php echo $this->get_input_param('postal_code') ?>"  
+                            data-gm-required onkeyup="AjaxZip3.zip2addr(this,'','address_1','address_2','address_3');">
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th><div>都道府県</div></th>
+                    <td>
+                        <div>
+                            <input class="gm-input" type="text" name="address_1" value="<?php echo $this->get_input_param('address_1') ?>"  
                             data-gm-required>
                         </div>
                     </td>
                 </tr>
                 <tr>
-                    <th><div>ご利用者様の属性</div></th>
+                    <th><div>市区町村</div></th>
                     <td>
-                        <div class="gm-radio-wrap">
-                            <?php
-                            foreach ($this->availability_records as $i => $record) {
-                                $checked = '';
-                                if ($this->get_input_param('availability_id') == $record->ID || (empty($this->get_input_param('availability_id')) && $i == 0)){
-                                    $checked = 'checked';
-                                }
-                                echo '<label><input type="radio" name="availability_id" value="' . $record->ID . '" ' . $checked . ' >' . $record->nm . '</label>';
-                                }
-                            ?>
+                        <div>
+                            <input class="gm-input" type="text" name="address_2" value="<?php echo $this->get_input_param('address_2') ?>"  
+                            data-gm-required>
                         </div>
                     </td>
                 </tr>
                 <tr>
-                    <th><div>ご不明な点など</div></th>
+                    <th><div>地番</div></th>
                     <td>
                         <div>
-                            <textarea class="gm-input" type="text" name="account_quiz" value="<?php echo $this->get_input_param('account_quiz') ?>"   
-                             data-gm-required><?php echo $this->get_input_param('account_quiz') ?></textarea>
+                            <input class="gm-input" type="text" name="address_3" value="<?php echo $this->get_input_param('address_3') ?>"  
+                            data-gm-required>
                         </div>
                     </td>
                 </tr>
+                <tr>
+                    <th><div>建物名・部屋番号</div></th>
+                    <td>
+                        <div>
+                            <input class="gm-input" type="text" name="address_4" value="<?php echo $this->get_input_param('address_4') ?>"  
+                            >
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th><div>アカウント属性</div></th>
+                    <td>
+                        <div class="gm-radio-wrap">
+                            <?php
+                                foreach ($this->account_attr_records as $i => $record) {
+                                    $checked = '';
+                                    if ($this->get_input_param('account_attr_id') == $record->ID || (empty($this->get_input_param('account_attr_id')) && $i == 0)){
+                                        $checked = 'checked';
+                                    }
+                                    echo '<label><input type="radio" name="account_attr_id" onchange="account_add()" value="' . $record->ID . '" ' . $checked . ' >' . $record->nm . '</label>';
+                                }
+                            ?>
+                            <div style="display: flex;">
+                                <label><input type="radio" name="account_attr_id" value="9" id="account_attr_id_other" onchange="account_other(this)">その他</label>
+                            </div>
+                        </div>
+                        <input class="gm-input-other" type="text" name="account_attr_other" id="account_attr_other" value="<?php echo $this->get_input_param('account_attr_other') ?>" disabled>
 
+                    </td>
+                </tr>
+                <tr>
+                    <th><div>連絡事項</div></th>
+                    <td>
+                        <div>
+                        <textarea class="gm-input" name="apply_memo" 
+                            ><?php echo $this->get_input_param('apply_memo') ?></textarea>
+                        </div>
+                    </td>
+                </tr>
             </table>
         </div>
         <div class="gm-input-button-wrap">
@@ -83,34 +132,25 @@ if (! defined('ABSPATH')) {
     </form>
 <?php elseif($this->mode == 'confirm') :?>
     <form id="gm-page-form" method="POST">
-        <input type="text" style="display:none" placeholder="Enter対策">
+        <input type="text" style="display:none" palceholder="Enter対策">
         <input type="hidden" name="process" value="regist">
         <div class="gm-input-table-wrap">
             <table class="gm-input-table">
                 <tr>
-                    <th><div>お名前</div></th>
+                    <th><div>名前</div></th>
                     <td>
                         <div>
-                            <input class="gm-input" type="text" name="account_nm" value="<?php echo $this->get_input_param('account_nm') ?>" disabled
-                            data-gm-required>
+                            <input class="gm-input" type="text" name="nm" value="<?php echo $this->get_input_param('nm') ?>" disabled
+                            >
                         </div>
                     </td>
                 </tr>
                 <tr>
-                    <th><div>住所</div></th>
+                    <th><div>カナ</div></th>
                     <td>
                         <div>
-                            <input class="gm-input" type="text" name="account_address" value="<?php echo $this->get_input_param('account_address') ?>"  disabled
-                            data-gm-required >
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <th><div>電話番号</div></th>
-                    <td>
-                        <div>
-                            <input class="gm-input" type="tel" name="account_tel" value="<?php echo $this->get_input_param('account_tel') ?>"  disabled
-                            data-gm-required>
+                            <input class="gm-input" type="text" name="kana" value="<?php echo $this->get_input_param('kana') ?>" disabled
+                            >
                         </div>
                     </td>
                 </tr>
@@ -118,37 +158,97 @@ if (! defined('ABSPATH')) {
                     <th><div>メールアドレス</div></th>
                     <td>
                         <div>
-                            <input class="gm-input" type="email" name="account_email" value="<?php echo $this->get_input_param('account_email') ?>"  disabled
+                            <input class="gm-input" type="text" name="email" value="<?php echo $this->get_input_param('email') ?>" disabled
+                            >
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th><div>電話番号</div></th>
+                    <td>
+                        <div class="gm-zipcode-part">
+                            <input class="gm-input" type="text" name="phone" value="<?php echo $this->get_input_param('phone') ?>"  
+                            data-gm-required data-gm-phone>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    
+                    <th><div>郵便番号</div></th>
+                    <td>
+                        <div class="gm-zipcode-part">
+                            <input class="gm-input2" type="text" name="postal_code" value="<?php echo $this->get_input_param('postal_code') ?>"  
                             data-gm-required>
                         </div>
                     </td>
                 </tr>
                 <tr>
-                    <th><div>ご利用者様の属性</div></th>
+                    <th><div>都道府県</div></th>
                     <td>
-                        <div class="gm-radio-wrap">
-                            <?php
-                            foreach ($this->availability_records as $i => $record) {
-                                $checked = '';
-                                if ($this->get_input_param('availability_id') == $record->ID || (empty($this->get_input_param('availability_id')) && $i == 0)){
-                                    $checked = 'checked';
-                                }
-                                echo '<label><input type="radio" name="availability_id" value="' . $record->ID . '" ' . $checked . ' disabled>' . $record->nm . '</label>';
-                                }
-                            ?>
+                        <div>
+                            <input class="gm-input" type="text" name="address_1" value="<?php echo $this->get_input_param('address_1') ?>" disabled
+                            >
                         </div>
                     </td>
                 </tr>
                 <tr>
-                    <th><div>ご不明な点など</div></th>
+                    <th><div>市区町村</div></th>
                     <td>
                         <div>
-                            <textarea class="gm-input" type="text" name="account_quiz" value="<?php echo $this->get_input_param('account_quiz') ?>"  disabled
-                            data-gm-required><?= $this->get_input_param('account_quiz') ?></textarea>
+                            <input class="gm-input" type="text" name="address_2" value="<?php echo $this->get_input_param('address_2') ?>" disabled
+                            >
                         </div>
                     </td>
                 </tr>
+                <tr>
+                    <th><div>地番</div></th>
+                    <td>
+                        <div>
+                            <input class="gm-input" type="text" name="address_3" value="<?php echo $this->get_input_param('address_3') ?>" disabled 
+                            >
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th><div>建物名・部屋番号</div></th>
+                    <td>
+                        <div>
+                            <input class="gm-input" type="text" name="address_4" value="<?php echo $this->get_input_param('address_4') ?>" disabled
+                            >
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th><div>アカウント属性</div></th>
+                    <td>
+                    <div class="gm-radio-wrap">
+                            <?php
+                            foreach ($this->account_attr_records as $i => $record) {
+                                $checked = '';
+                                if ($this->get_input_param('account_attr_id') == $record->ID || (empty($this->get_input_param('account_attr_id')) && $i == 0)){
+                                    $checked = 'checked';
+                                }
+                                echo '<label><input type="radio" name="account_attr_id" value="' . $record->ID . '" ' . $checked . '  disabled>' . $record->nm . '</label>';
+                                }
+                            ?>
+                            <div style="display: flex;">
+                                <label><input type="radio" name="account_attr_id" value="9" disabled checked=<?= $this->account_other; ?>>その他</label>
+                            </div>
 
+                        </div>
+                        <input class="gm-input" type="text" name="account_attr_other" value="<?php echo $this->get_input_param('account_attr_other') ?>" disabled>
+
+                    </td>
+                </tr>
+                <tr>
+                    <th><div>連絡事項</div></th>
+                    <td>
+                        <div>
+                        <textarea class="gm-input" name="apply_memo" disabled
+                            ><?php echo $this->get_input_param('apply_memo') ?></textarea>
+                        </div>
+                    </td>
+                </tr>
             </table>
         </div>
         <div class="gm-input-button-wrap">
@@ -170,3 +270,16 @@ if (! defined('ABSPATH')) {
         </div>
 <?php endif; ?>
 </div>
+
+
+<script>
+    function account_other() {
+        document.getElementById("account_attr_other").disabled = false;
+    }
+
+    function account_add() {
+        document.getElementById("account_attr_other").disabled = true;
+    }
+    
+    
+</script>
