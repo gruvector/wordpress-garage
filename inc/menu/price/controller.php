e4063502fa583622d30ecbaac52fea9f787f8edf
gruvector <vector.gru316@gmail.com>
2024-02-07 12:02:38 +0900

    correction 22

---
+++ inc/menu/price/controller.php
@@ -0,0 +1,432 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+require_once ABSPATH . 'wp-content/themes/sango-theme-child-garage/inc/menu/abstract-menu-controller.php';
+class Gm_Property_Menu_Controller extends Gm_Abstract_List_Menu_Controller
+{
+    public $area_list = [];
+    public $service;
+    public $show_mode;
+    public $show_modal = 'none';
+    public $show_data;
+
+    /** コンストラクタ */
+    public function __construct()
+    {
+        require_once plugin_dir_path(__FILE__) . 'service.php';
+        $this->service = new Gm_Property_Menu_Service();
+        $table = new Gm_Property_Menu_Table();
+        $keys = array('ID');
+        parent::__construct($this->service, $table, $keys);
+    }
+
+    /**
+     * 実行処理
+     * @override
+     */
+    public function action()
+    {
+        if (isset($_POST['process'])) {
+            if ($_POST['process'] == 'edit') {
+                $this->show_data = $this->service->edit(isset($_POST['execute_id']) ? $_POST['execute_id'] : null);
+                $this->show_modal = 'block';
+            } elseif ($_POST['process'] == 'ban') {
+                $this->service->deny(isset($_POST['execute_id']) ? $_POST['execute_id'] : null);
+            } elseif ($_POST['process'] == 'recover') {
+                $this->service->recover(isset($_POST['execute_id']) ? $_POST['execute_id'] : null);
+            } elseif ($_POST['process1'] == 'apply') {
+                $this->service->apply(isset($_POST) ? $_POST : null);
+            } 
+        }
+
+        $param_s = (isset($_REQUEST['s'])) ? $_REQUEST['s'] : '';
+        $param_orderby = (isset($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : false;
+        $param_order = (isset($_REQUEST['order'])) ? $_REQUEST['order'] : '';
+        $this->show_mode = $this->service->show_mode = (isset($_REQUEST['show_mode'])) ? $_REQUEST['show_mode'] : '';
+
+        $records = $this->service->get_list($param_s, $param_orderby, $param_order);
+        $this->table->prepare_items($records);
+        $list = $this->parse_item_to_list($this->table->items);
+        $this->list = json_encode($list, JSON_UNESCAPED_UNICODE);
+        $this->render_view();
+    }
+
+    /** view表示 */
+    protected function render_view()
+    {
+        require plugin_dir_path(__FILE__) . '/view.php';
+    }
+}
+
+/****************************************************
+/ 以下、テーブル用
+*****************************************************/
+class Gm_Property_Menu_Table extends Gm_Abstract_Menu_Table
+{
+    /** コンストラクタ */
+    public function __construct()
+    {
+        $keys = array('ID');
+        $per_page = 40;
+        parent::__construct($keys, $per_page);
+    }
+
+    /** 列名 */
+    public function get_columns()
+    {
+        $columns = array(
+            'ID' => 'ID',
+            'account_id' => 'アカウントID',
+            'property_id' => '物件ID',
+            'nm' => '名前',
+            'section_nm' => '区画名称',
+            'availability_id' => '空き状況',
+            'handover_date' => '引き渡し可能日',
+            'min_period' => '最低契約期間',
+            'min_period_unit' => '最低契約期間単位',
+            'size_w' => '横幅',
+            'size_h' => '高さ',
+            'size_d' => '奥行',
+            'fee_monthly_rent' => '賃料',
+            'fee_monthly_common_service' => '共益費',
+            'fee_monthly_others' => 'その他',
+            'fee_contract_security' => '敷金',
+            'fee_contract_security_amortization' => '敷金償却',
+            'fee_contract_deposit' => '保証金',
+            'fee_contract_deposit_amortization' => '保証金償却',
+            'fee_contract_key_money' => '礼金',
+            'fee_contract_guarantee_charge' => '保証料',
+            'fee_contract_other' => 'その他',
+            'facility_ids' => '設備情報',
+            'other_description' => 'その他紹介',
+            'appeal_description' => 'アピールポイント',
+            'address_1' => '都道府県',
+            'address_1' => '市区町村',
+            'address_1' => '地番',
+            'address_1' => '建物名',
+            'created_at' => '登録日時',
+            'updated_at' => '更新日時',
+
+        );
+        return $columns;
+    }
+
+    public function get_sortable_columns()
+    {
+        $sortable_columns = array(
+            // MEMO 列名 => array(カラム名, 初期ソート可否)
+            'ID' => array('ID',false),
+        );
+        return $sortable_columns;
+    }
+
+    /** データ生成 */
+    protected function create_data($items)
+    {
+        return Gm_Property_Menu_Info::create_data($items);
+    }
+
+    /*******************
+    / 列情報
+    ********************/
+    public function column_ID($item)
+    {
+        if ( $item->get_status1() == '1' ) {
+            return <<<EOM
+                <div>{$item->get_ID()}</div>
+                <div class="gm-admin-button-wrap">
+                <button type="button" class="gm-admin-button-apply" onClick="document.getElementsByName('process')[0].value='edit';document.getElementsByName('execute_id')[0].value='{$item->get_ID()}'; document.getElementById('gm-admin-form').submit();">編集</button>
+                <button type="button" class="gm-admin-button-deny" onClick="document.getElementsByName('process')[0].value='ban';document.getElementsByName('execute_id')[0].value='{$item->get_ID()}'; document.getElementById('gm-admin-form').submit();">BAN</button>
+                </div>
+                EOM;
+        } else {
+            return <<<EOM
+                <div>{$item->get_ID()}</div>
+                <div class="gm-admin-button-wrap">
+                <button type="button" class="gm-admin-button-apply" onClick="document.getElementsByName('process')[0].value='edit';document.getElementsByName('execute_id')[0].value='{$item->get_ID()}'; document.getElementById('gm-admin-form').submit();">編集</button>
+                <button type="button" class="gm-admin-button-deny" onClick="document.getElementsByName('process')[0].value='recover';document.getElementsByName('execute_id')[0].value='{$item->get_ID()}'; document.getElementById('gm-admin-form').submit();">RECOVER</button>
+                </div>
+                EOM;
+        }
+    }
+
+    public function column_property_id($item)
+    {
+        return $item->get_property_id();
+    }
+
+    public function column_account_id($item)
+    {
+        return $item->get_account_id();
+    }
+    
+    public function column_nm($item)
+    {
+        return $item->get_nm();
+    }
+
+    public function column_section_nm($item)
+    {
+        return $item->get_section_nm();
+    }
+
+    public function column_availability_id($item)
+    {
+        return $item->get_availability_id();
+    }
+    
+    public function column_handover_date($item)
+    {
+        return $item->get_handover_date();
+    }
+    
+    public function column_min_period($item)
+    {
+        return $item->get_min_period();
+    }
+    
+    public function column_min_period_unit($item)
+    {
+        return $item->get_min_period_unit();
+    }
+    
+    public function column_size_w($item)
+    {
+        return $item->get_size_w();
+    }
+    
+    public function column_size_h($item)
+    {
+        return $item->get_size_h();
+    }
+    
+    public function column_size_d($item)
+    {
+        return $item->get_size_d();
+    }
+    
+    public function column_fee_monthly_rent($item)
+    {
+        return $item->get_fee_monthly_rent();
+    }
+ 
+    public function column_fee_monthly_common_service($item)
+    {
+        return $item->get_fee_monthly_common_service();
+    }
+ 
+    public function column_fee_monthly_others($item)
+    {
+        return $item->get_fee_monthly_others();
+    }
+ 
+    public function column_fee_contract_security($item)
+    {
+        return $item->get_fee_contract_security();
+    }
+
+    public function column_fee_contract_deposit($item)
+    {
+        return $item->get_fee_contract_deposit();
+    }
+ 
+    public function column_fee_contract_security_amortization($item)
+    {
+        return $item->get_fee_contract_security_amortization();
+    }
+
+    public function column_fee_contract_deposit_amortization($item)
+    {
+        return $item->get_fee_contract_deposit_amortization();
+    }
+ 
+    public function column_fee_contract_key_money($item)
+    {
+        return $item->get_fee_contract_key_money();
+    }
+
+    public function column_fee_contract_guarantee_charge($item)
+    {
+        return $item->get_fee_contract_guarantee_charge();
+    }
+
+    public function column_fee_contract_other($item)
+    {
+        return $item->get_fee_contract_other();
+    }
+
+    public function column_facility_ids($item)
+    {
+        return $item->get_facility_ids();
+    }
+
+    public function column_other_description($item)
+    {
+        return $item->get_other_description();
+    }
+
+    public function column_appeal_description($item)
+    {
+        return $item->get_appeal_description();
+    }
+
+    public function column_postal_code($item)
+    {
+        return $item->get_postal_code();
+    }
+
+    public function column_address_1($item)
+    {
+        return $item->get_address_1();
+    }
+
+    public function column_address_2($item)
+    {
+        return $item->get_address_2();
+    }
+
+    public function column_address_3($item)
+    {
+        return $item->get_address_3();
+    }
+
+    public function column_address_4($item)
+    {
+        return $item->get_address_4();
+    }
+
+    public function column_created_at($item)
+    {
+        return $item->get_created_at();
+    }
+
+    // public function column_status1($item)
+    // {
+    //     return $item->get_status1();
+    // }
+
+}
+
+class Gm_Property_Menu_Item extends Gm_Abstract_Menu_Item
+{
+    public function __construct($record)
+    {
+        $this->ID = $record->ID;
+        $this->property_id = $record->property_id;
+        $this->account_id = $record->account_id;
+        $this->nm = $record->nm;
+        $this->section_nm = $record->section_nm;
+        $this->availability_id = $record->availability_id;
+        $this->handover_date = $record->handover_date;
+        $this->min_period = $record->min_period;
+        $this->min_period_unit = $record->min_period_unit;
+        $this->size_w = $record->size_w;
+        $this->size_h = $record->size_h;
+        $this->size_d = $record->size_d;
+        $this->fee_monthly_rent = $record->fee_monthly_rent;
+        $this->fee_monthly_common_service = $record->fee_monthly_common_service;
+        $this->fee_monthly_others = $record->fee_monthly_others;
+        $this->fee_contract_security = $record->fee_contract_security;
+        $this->fee_contract_security_amortization = $record->fee_contract_security_amortization;
+        $this->fee_contract_deposit = $record->fee_contract_deposit;
+        $this->fee_contract_deposit_amortization = $record->fee_contract_deposit_amortization;
+        $this->fee_contract_key_money = $record->fee_contract_key_money;
+        $this->fee_contract_guarantee_charge = $record->fee_contract_guarantee_charge;
+        $this->fee_contract_other = $record->fee_contract_other;
+        $this->facility_ids = $record->facility_ids;
+        $this->other_description = $record->other_description;
+        $this->appeal_description = $record->appeal_description;
+        $this->postal_code = $record->postal_code;
+        $this->address_1 = $record->address_1;
+        $this->address_2 = $record->address_2;
+        $this->address_3 = $record->address_3;
+        $this->address_4 = $record->address_4;
+        $this->status1 = $record->status1;
+        $this->created_at = $record->created_at;
+    }
+
+    protected $ID;
+    protected $property_id;
+    protected $account_id;
+    protected $nm;
+    protected $section_nm;
+    protected $imgs;
+    protected $availability_id;
+    protected $handover_date;
+    protected $min_period;
+    protected $min_period_unit;
+    protected $size_w;
+    protected $size_h;
+    protected $size_d;
+    protected $fee_monthly_rent;
+    protected $fee_monthly_common_service;
+    protected $fee_monthly_others;
+    protected $fee_contract_security;
+    protected $fee_contract_security_amortization;
+    protected $fee_contract_deposit;
+    protected $fee_contract_deposit_amortization;
+    protected $fee_contract_key_money;
+    protected $fee_contract_guarantee_charge;
+    protected $fee_contract_other;
+    protected $facility_ids;
+    protected $other_description;
+    protected $appeal_description;
+    protected $postal_code;
+    protected $address_1;
+    protected $address_2;
+    protected $address_3;
+    protected $address_4;
+    protected $status1;
+    protected $created_at;
+
+
+    public function get_ID(){return $this->ID;}
+    public function get_property_id(){return $this->property_id;}
+    public function get_account_id() {return $this->account_id;}
+    public function get_nm(){return $this->nm;}
+    public function get_section_nm(){return $this->section_nm;}
+    public function get_availability_id(){return $this->availability_id;}
+    public function get_handover_date(){return $this->handover_date;}
+    public function get_min_period(){return $this->min_period;}
+    public function get_min_period_unit(){return $this->min_period_unit;}
+    public function get_size_w(){return $this->size_w;}
+    public function get_size_h(){return $this->size_h;}
+    public function get_size_d(){return $this->size_d;}
+    public function get_fee_monthly_rent(){return $this->fee_monthly_rent;}
+    public function get_fee_monthly_common_service(){return $this->fee_monthly_common_service;}
+    public function get_fee_monthly_others(){return $this->fee_monthly_others;}
+    public function get_fee_contract_security(){return $this->fee_contract_security;}
+    public function get_fee_contract_security_amortization(){return $this->fee_contract_security_amortization;}
+    public function get_fee_contract_deposit(){return $this->fee_contract_deposit;}
+    public function get_fee_contract_deposit_amortization(){return $this->fee_contract_deposit_amortization;}
+    public function get_fee_contract_key_money(){return $this->fee_contract_key_money;}
+    public function get_fee_contract_guarantee_charge(){return $this->fee_contract_guarantee_charge;}
+    public function get_fee_contract_other(){return $this->fee_contract_other;}
+    public function get_facility_ids(){return $this->facility_ids;}
+    public function get_other_description(){return $this->other_description;}
+    public function get_appeal_description(){return $this->appeal_description;}
+    public function get_postal_code(){return $this->postal_code;}
+    public function get_address_1(){return $this->address_1;}
+    public function get_address_2(){return $this->address_2;}
+    public function get_address_3(){return $this->address_3;}
+    public function get_address_4(){return $this->address_4;}
+    public function get_status1(){return $this->status1;}
+    public function get_created_at () {return $this->created_at;}
+
+}
+
+class Gm_Property_Menu_Info extends Gm_Abstract_Menu_Info
+{
+    public static function create_data($records)
+    {
+        $items = [];
+        if (is_array($records)) {
+            foreach ($records as $record) {
+                $items[] = new Gm_Property_Menu_Item($record);
+            }
+        }
+        return new Gm_Property_Menu_Info($GLOBALS['title'], $items);
+    }
+}
---
+++ inc/menu/price/service.php
@@ -0,0 +1,194 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+require_once ABSPATH . 'wp-content/themes/sango-theme-child-garage/inc/menu/abstract-menu-service.php';
+class Gm_Property_Menu_Service extends Gm_Abstract_Menu_Service
+{
+    public $show_mode;
+    public $data_show;
+    public $check_box = [];
+    
+
+    public function __construct()
+    {
+        $conf = new Gm_Menu_Service_Conf();
+        $conf->set_main_table('gmt_property');
+        $conf->set_main_table_alias('property');
+        $conf->set_add_cond_search('property.nm like \'%$s%\' OR property.postal_code like \'%$s%\' OR property.address_1 like \'%$s%\' OR property.address_2 like \'%$s%\' OR property.address_3 like \'%$s%\' OR property.address_4 like \'%$s%\'');
+        $conf->set_order_init('ID DESC');
+
+        parent::__construct($conf);
+    }
+    // -----------------------------------------------
+    // 更新系
+    // -----------------------------------------------
+
+    public function edit($ID)
+    {
+        if (empty($ID)) {
+            return;
+        }
+        $record = [];
+        global $wpdb;
+        $records1 = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}gmt_property WHERE ID = {$ID}");
+        $records2 = $wpdb->get_results("SELECT ID, nm FROM {$wpdb->prefix}gmm_availability order by priority");
+        $records3 = $wpdb->get_results("SELECT ID, nm FROM {$wpdb->prefix}gmm_facility order by priority");
+        if (empty($records1) || empty($records2)){
+            return;
+        }
+        $record[0] = $records1[0];
+        $record[1] = $records2;
+        $record[2] = $records3;
+        return $record;
+
+    }
+
+    public function apply($ID)
+    {
+        if (empty($ID)) {
+            return;
+        }
+        $this->data_show = $_POST;
+        global $wpdb;
+        var_dump($this->data_show['facility_id']);
+        for ($i = 0; $i < 12 ; $i++) { 
+            if(isset($this->data_show['facility_id'][$i])) {
+                array_push($this->check_box, $this->data_show['facility_id'][$i]);
+           }
+        }
+        $a = implode(",", $this->check_box);
+        $wpdb->update(
+            $wpdb->prefix.'gmt_property',
+            [
+                'property_id' => $this->data_show['property_id'],
+                'nm' => $this->data_show['nm'],
+                'section_nm' => $this->data_show['section_nm'],
+                'availability_id' => $this->data_show['availability_id'],
+                'handover_date' => $this->data_show['handover_date'],
+                'min_period' => $this->data_show['min_period'],
+                'min_period_unit' => $this->data_show['min_period_unit'],
+                'size_w' => $this->data_show['size_w'],
+                'size_h' => $this->data_show['size_h'],
+                'size_d' => $this->data_show['size_d'],
+                'fee_monthly_rent' => $this->data_show['fee_monthly_rent'],
+                'fee_monthly_common_service' => $this->data_show['fee_monthly_common_service'],
+                'fee_monthly_others' => $this->data_show['fee_monthly_others'],
+                'fee_contract_security' => $this->data_show['fee_contract_security'],
+                'fee_contract_security_amortization' => $this->data_show['fee_contract_security_amortization'],
+                'fee_contract_deposit' => $this->data_show['fee_contract_deposit'],
+                'fee_contract_deposit_amortization' => $this->data_show['fee_contract_deposit_amortization'],
+                'fee_contract_key_money' => $this->data_show['fee_contract_key_money'],
+                'fee_contract_guarantee_charge' => $this->data_show['fee_contract_guarantee_charge'],
+                'fee_contract_other' => $this->data_show['fee_contract_other'],
+                'facility_ids' => $a,
+                'other_description' => $this->data_show['other_description'],
+                'appeal_description' => $this->data_show['appeal_description'],
+                'postal_code' => $this->data_show['postal_code'],
+                'address_1' => $this->data_show['address_1'],
+                'address_2' => $this->data_show['address_2'],
+                'address_3' => $this->data_show['address_3'],
+                'address_4' => $this->data_show['address_4'],
+            ],
+            [
+                'ID' => $this->data_show['ID1'],
+            ]
+        );
+
+    }
+
+    public function deny($ID)
+    {
+        if (empty($ID)) {
+            return;
+        }
+        global $wpdb;
+        $wpdb->update(
+            $wpdb->prefix . 'gmt_property',
+            ['status1' => '0',],
+            ['ID' => $ID],
+            ['%d'],
+        );
+    }
+
+    public function recover($ID)
+    {
+        if (empty($ID)) {
+            return;
+        }
+        global $wpdb;
+        $wpdb->update(
+            $wpdb->prefix . 'gmt_property',
+            ['status1' => '1',],
+            ['ID' => $ID],
+            ['%d'],
+        );
+    }
+
+    // -----------------------------------------------
+    // 参照系
+    // -----------------------------------------------
+    /** 一覧取得（メイン処理） */
+    public function get_list_with_cond($add_cond = '', $order = '')
+    {
+        $sql_order = $this->order_init;
+        if (!empty($order)) {
+            $sql_order = $order;
+        }
+
+        $add_cond .= ' AND property.status1 = ' . (($this->show_mode == '9') ? '0' : '1');
+
+        global $wpdb;
+        $sql =
+        <<<EOM
+        SELECT
+            property.ID
+            , property.account_id
+            , property.property_id
+            , property.nm
+            , property.section_nm
+            , property.availability_id
+            , property.handover_date
+            , property.min_period
+            , property.min_period_unit
+            , property.size_w
+            , property.size_h
+            , property.size_d
+            , property.fee_monthly_rent
+            , property.fee_monthly_common_service
+            , property.fee_monthly_others
+            , property.fee_contract_security
+            , property.fee_contract_security_amortization
+            , property.fee_contract_deposit
+            , property.fee_contract_deposit_amortization
+            , property.fee_contract_key_money
+            , property.fee_contract_guarantee_charge
+            , property.fee_contract_other
+            , property.facility_ids
+            , property.other_description
+            , property.appeal_description
+            , property.postal_code
+            , property.address_1
+            , property.address_2
+            , property.address_3
+            , property.address_4
+            , property.status1
+            , property.created_at
+        FROM
+            {$wpdb->prefix}gmt_property AS property
+            LEFT JOIN {$wpdb->prefix}gmm_availability AS availability
+                ON property.availability_id = availability.ID
+            LEFT JOIN {$wpdb->prefix}gmm_facility AS facility
+                ON property.facility_ids = facility.ID
+        WHERE 1 = 1 {$add_cond}
+        ORDER BY {$sql_order};
+        EOM;
+
+        $records = $wpdb->get_results($sql);
+        return $records;
+  
+    
+    }
+
+}
